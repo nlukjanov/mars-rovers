476bcbb40f58d0ac9deec7f7d1be03bfb2242493
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-08-21 16:42:48 +0300

    refactor into separate functions

--- marsRovers.js
+++ marsRovers.js
@@ -1,3 +1,45 @@
+const roverMove = (roverLocation) => {
+  if (roverLocation.d === 'N') {
+    roverLocation.y += 1;
+  } else if (roverLocation.d === 'E') {
+    roverLocation.x += 1;
+  } else if (roverLocation.d === 'S') {
+    roverLocation.y -= 1;
+  } else if (roverLocation.d === 'W') {
+    roverLocation.x -= 1;
+  }
+  return roverLocation;
+};
+
+const roverLeftRotation = {
+  N: 'W',
+  W: 'S',
+  S: 'E',
+  E: 'N'
+};
+
+const roverRightRotation = {
+  N: 'E',
+  E: 'S',
+  S: 'W',
+  W: 'N'
+};
+
+const calculatePath = (roverData, roverInstruction) => {
+  let roverLocation = { ...roverData };
+  for (let index = 0; index < roverInstruction.length; index++) {
+    const element = roverInstruction[index];
+    if (element === 'L') {
+      roverLocation.d = roverLeftRotation[roverLocation.d];
+    } else if (element === 'R') {
+      roverLocation.d = roverRightRotation[roverLocation.d];
+    } else if (element === 'M') {
+      roverLocation = roverMove(roverLocation);
+    }
+  }
+  return roverLocation;
+};
+
 const marsExploration = (input) => {
   const {
     rover1Location,
@@ -6,50 +48,8 @@ const marsExploration = (input) => {
     rover2Instruction
   } = input;
 
-  const roverLeftRotation = {
-    N: 'W',
-    W: 'S',
-    S: 'E',
-    E: 'N'
-  };
-
-  const roverRightRotation = {
-    N: 'E',
-    E: 'S',
-    S: 'W',
-    W: 'N'
-  };
-
-  const roverMove = (roverLocation) => {
-    if (roverLocation.d === 'N') {
-      roverLocation.y += 1;
-    } else if (roverLocation.d === 'E') {
-      roverLocation.x += 1;
-    } else if (roverLocation.d === 'S') {
-      roverLocation.y -= 1;
-    } else if (roverLocation.d === 'W') {
-      roverLocation.x -= 1;
-    }
-    return roverLocation;
-  };
-
   const rover1InstructionArray = rover1Instruction.split('');
   const rover2InstructionArray = rover2Instruction.split('');
-
-  const calculatePath = (roverData, roverInstruction) => {
-    let roverLocation = { ...roverData };
-    for (let index = 0; index < roverInstruction.length; index++) {
-      const element = roverInstruction[index];
-      if (element === 'L') {
-        roverLocation.d = roverLeftRotation[roverLocation.d];
-      } else if (element === 'R') {
-        roverLocation.d = roverRightRotation[roverLocation.d];
-      } else if (element === 'M') {
-        roverLocation = roverMove(roverLocation);
-      }
-    }
-    return roverLocation;
-  };
 
   const rover1FinalLocation = calculatePath(
     rover1Location,
@@ -64,4 +64,4 @@ const marsExploration = (input) => {
   return { rover1: rover1FinalLocation, rover2: rover2FinalLocation };
 };
 
-module.exports = marsExploration;
+module.exports = { marsExploration, roverMove, calculatePath };
