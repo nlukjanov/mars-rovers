9b6ba01a0df56a0e7de7cb5db1c5ea07d56020f6
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-08-21 16:09:45 +0300

    add test for rovers final location

--- marsRovers.test.js
+++ marsRovers.test.js
@@ -1,11 +1,22 @@
-const exploration = require('./marsRovers');
+const marsExploration = require('./marsRovers');
 
 describe('Testing Mars Rovers exploration', () => {
+  const input = {
+    grid: { x: 5, y: 5 },
+    rover1Location: { x: 1, y: 2, d: 'N' },
+    rover1Instruction: 'LMLMLMLMM',
+    rover2Location: { x: 3, y: 3, d: 'E' },
+    rover2Instruction: 'MMRMMRMRRM'
+  };
+
   test('should have a function', () => {
-    expect(exploration).toBeTruthy();
+    expect(marsExploration).toBeTruthy();
   });
 
-  test('should return an object with 2 Rovers', () => {
-    expect(exploration()).toEqual({ rover1: {}, rover2: {} });
+  test('should return correct calculation for rovers', () => {
+    expect(marsExploration(input)).toMatchObject({
+      rover1: { x: 1, y: 3, d: 'N' },
+      rover2: { x: 5, y: 1, d: 'E' }
+    });
   });
 });
